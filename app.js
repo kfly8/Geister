bed2e4f2d222cd4f55a6574ac328ff77a9a25c74
Kenta, Kobayashi <kentafly88@gmail.com>
2013-11-16 22:37:13 +0900

    herokuでsocket.io使う周りの設定 https://devcenter.heroku.com/articles/realtime-polyglot-app-node-ruby-mongodb-socketio#pushing-messages-to-the-browser-with-socket-io

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ var app    = require('express')()
   , server = require('http').createServer(app)
   , io     = require('socket.io').listen(server);
 
+
+// https://devcenter.heroku.com/articles/realtime-polyglot-app-node-ruby-mongodb-socketio#pushing-messages-to-the-browser-with-socket-io
+io.configure(function () {
+  io.set("transports", ["xhr-polling"]);
+  io.set("polling duration", 10);
+});
+
 server.listen(5000);
 
 app.get('/', function (req, res) {

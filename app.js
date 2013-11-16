08e9fcbad178dc9a536879ecd6b843077af01a91
Kenta, Kobayashi <kentafly88@gmail.com>
2013-11-16 21:34:15 +0900

    change port

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var app    = require('express')()
   , server = require('http').createServer(app)
   , io     = require('socket.io').listen(server);
 
-server.listen(80);
+server.listen(5000);
 
 app.get('/', function (req, res) {
   res.sendfile(__dirname + '/index.html');
